9d27ca24bd4454b104b2a17997c8dd4c608befd0
sampathkumar-akula <74893958+sampathkumar-akula@users.noreply.github.com>
2020-12-15 19:57:19 +0530

    restaurant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
